5e2b270b0b55c0b646be22da0972d554ada32f49
Aman Kumar <vrohan319@gmail.com>
2022-10-17 00:59:47 +0530

    added itemAddHandler and manage its state useReducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,42 @@
 import CartConext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action === "ADD") {
+    const updatedItems = state.item.concate(action.item);
+    const updatedTotalAmount =
+      state.item.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      item: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState ;
+};
 
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-  const removeItemFromCartHandler = (id) => {};
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
